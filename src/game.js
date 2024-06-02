b4334df24ae6a4d9e8c253b080ddb5290b5d5503
Michael O’Sullivan <michaelosull@protonmail.ch>
2024-06-02 16:43:07 -0400

    Set up basic functionality to add and remove letters from a guess

--- src/game.js
+++ src/game.js
@@ -20,7 +20,8 @@ const fiveLetterWords = [
     "cupbo", "bench", "glass", "blinds", "frame"
 ];
   
-
+let currentWord = fiveLetterWords[Math.floor(Math.random() * fiveLetterWords.length)];
+let currentGuess = []
 let numberOfGuessesLeft = 6;
 
 function createLayout() {
@@ -42,5 +43,60 @@ function createLayout() {
         }
     }
 }
+
+// Helper function for adding/removing letters from the UI
+function setLetter(letter) {
+    let rowNum = 6 - numberOfGuessesLeft;
+    let letterNum = currentGuess.length - 1;
+    const letterElement = document.getElementById("row"+rowNum+"Letter"+letterNum);
+    letterElement.innerHTML = letter;
+}
+
+// Add a letter to the current guess
+function addLetter(letter) {
+    if(currentGuess.length >= 5) {
+        console.log("Word is already 5 letters long");
+        return;
+    }
+    currentGuess.push(letter);
+    setLetter(letter);
+}
+
+// Remove a letter to the current guess
+function deleteLetter() {
+    if(currentGuess.length <= 0) {
+        console.log("Word is already empty");
+        return;
+    }
+    // Pop from array after UI deletion to avoid index errors
+    setLetter("")
+    currentGuess.pop()
+}
+
+// Submit a word as a guess
+function submitWord() {
+    if(currentGuess.length != 5) {
+        console.log("Word is not 5 letters long");
+        return;
+    }
+    numberOfGuessesLeft--;
+    currentGuess = [];
+    // TODO
+}
+
+function handleKeyPress(event) {
+    // Check if the pressed key is a letter
+    if (event.key.match(/^[a-zA-Z]$/)) {
+        addLetter(event.key.toUpperCase());
+    } else if (event.key === "Backspace") {
+        deleteLetter();
+    } else if(event.key === "Enter") {
+        submitWord();
+    } else {
+        return;
+    }
+  }
+
+window.addEventListener("keydown", handleKeyPress);
 
 createLayout();
